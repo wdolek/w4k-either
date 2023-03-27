26e41c83e350843a0302aa144609e887bf2dd430
Zdenek Havlin <wdolek@hotmail.com>
2023-03-27 23:27:47 +0200

    Introduce `W4k.Either.CodeGenerator`

---
+++ src/W4k.Either.CodeGenerator/EitherGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.CodeAnalysis;
+
+namespace W4k.Either.CodeGenerator;
+
+[Generator]
+public class EitherGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
