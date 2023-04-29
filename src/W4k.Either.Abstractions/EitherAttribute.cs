b3b1f9bca7092383de0e580744243078e112c71c
Zdenek Havlin <wdolek@hotmail.com>
2023-04-29 23:32:58 +0200

    Add XML doc

--- src/W4k.Either.Abstractions/EitherAttribute.cs
+++ src/W4k.Either.Abstractions/EitherAttribute.cs
@@ -23,43 +23,78 @@ namespace W4k.Either.Abstractions;
 [AttributeUsage(AttributeTargets.Struct, AllowMultiple = false, Inherited = false)]
 public class EitherAttribute : Attribute
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="EitherAttribute"/> class.
+    /// </summary>
     public EitherAttribute()
     {
     }
 
-    public EitherAttribute(Type t0, Type t1)
+    /// <summary>
+    /// Initializes a new instance of the <see cref="EitherAttribute"/> class.
+    /// </summary>
+    /// <remarks>
+    /// Depending on nullable reference type scope, reference type may represent either nullable or non-nullable type.
+    /// When <c>nullable</c> is enabled, reference type is not nullable, otherwise it is considered nullable (<c>?</c>).
+    /// </remarks>
+    /// <param name="t1">Type of first value.</param>
+    /// <param name="t2">Type of second value.</param>
+    public EitherAttribute(Type t1, Type t2)
     {
-        ThrowHelper.ThrowIfNull(t0);
-        ThrowHelper.ThrowIfNull(t1);
-    }
-
-    public EitherAttribute(Type t0, Type t1, Type t2)
-    {
-        ThrowHelper.ThrowIfNull(t0);
         ThrowHelper.ThrowIfNull(t1);
         ThrowHelper.ThrowIfNull(t2);
     }
 
-    public EitherAttribute(Type t0, Type t1, Type t2, Type t3)
+    /// <summary>
+    /// Initializes a new instance of the <see cref="EitherAttribute"/> class.
+    /// </summary>
+    /// <remarks>
+    /// Depending on nullable reference type scope, reference type may represent either nullable or non-nullable type.
+    /// When <c>nullable</c> is enabled, reference type is not nullable, otherwise it is considered nullable (<c>?</c>).
+    /// </remarks>
+    /// <param name="t1">Type of first value.</param>
+    /// <param name="t2">Type of second value.</param>
+    /// <param name="t3">Type of third value.</param>
+    public EitherAttribute(Type t1, Type t2, Type t3)
     {
-        ThrowHelper.ThrowIfNull(t0);
         ThrowHelper.ThrowIfNull(t1);
         ThrowHelper.ThrowIfNull(t2);
         ThrowHelper.ThrowIfNull(t3);
     }
 
-    public EitherAttribute(Type t0, Type t1, Type t2, Type t3, Type t4)
+    /// <summary>
+    /// Initializes a new instance of the <see cref="EitherAttribute"/> class.
+    /// </summary>
+    /// <remarks>
+    /// Depending on nullable reference type scope, reference type may represent either nullable or non-nullable type.
+    /// When <c>nullable</c> is enabled, reference type is not nullable, otherwise it is considered nullable (<c>?</c>).
+    /// </remarks>
+    /// <param name="t1">Type of first value.</param>
+    /// <param name="t2">Type of second value.</param>
+    /// <param name="t3">Type of third value.</param>
+    /// <param name="t4">Type of fourth value.</param>
+    public EitherAttribute(Type t1, Type t2, Type t3, Type t4)
     {
-        ThrowHelper.ThrowIfNull(t0);
         ThrowHelper.ThrowIfNull(t1);
         ThrowHelper.ThrowIfNull(t2);
         ThrowHelper.ThrowIfNull(t3);
         ThrowHelper.ThrowIfNull(t4);
     }
 
-    public EitherAttribute(Type t0, Type t1, Type t2, Type t3, Type t4, Type t5)
+    /// <summary>
+    /// Initializes a new instance of the <see cref="EitherAttribute"/> class.
+    /// </summary>
+    /// <remarks>
+    /// Depending on nullable reference type scope, reference type may represent either nullable or non-nullable type.
+    /// When <c>nullable</c> is enabled, reference type is not nullable, otherwise it is considered nullable (<c>?</c>).
+    /// </remarks>
+    /// <param name="t1">Type of first value.</param>
+    /// <param name="t2">Type of second value.</param>
+    /// <param name="t3">Type of third value.</param>
+    /// <param name="t4">Type of fourth value.</param>
+    /// <param name="t5">Type of fifth value.</param>
+    public EitherAttribute(Type t1, Type t2, Type t3, Type t4, Type t5)
     {
-        ThrowHelper.ThrowIfNull(t0);
         ThrowHelper.ThrowIfNull(t1);
         ThrowHelper.ThrowIfNull(t2);
         ThrowHelper.ThrowIfNull(t3);
@@ -67,9 +102,21 @@ public class EitherAttribute : Attribute
         ThrowHelper.ThrowIfNull(t5);
     }
 
-    public EitherAttribute(Type t0, Type t1, Type t2, Type t3, Type t4, Type t5, Type t6)
+    /// <summary>
+    /// Initializes a new instance of the <see cref="EitherAttribute"/> class.
+    /// </summary>
+    /// <remarks>
+    /// Depending on nullable reference type scope, reference type may represent either nullable or non-nullable type.
+    /// When <c>nullable</c> is enabled, reference type is not nullable, otherwise it is considered nullable (<c>?</c>).
+    /// </remarks>
+    /// <param name="t1">Type of first value.</param>
+    /// <param name="t2">Type of second value.</param>
+    /// <param name="t3">Type of third value.</param>
+    /// <param name="t4">Type of fourth value.</param>
+    /// <param name="t5">Type of fifth value.</param>
+    /// <param name="t6">Type of sixth value.</param>
+    public EitherAttribute(Type t1, Type t2, Type t3, Type t4, Type t5, Type t6)
     {
-        ThrowHelper.ThrowIfNull(t0);
         ThrowHelper.ThrowIfNull(t1);
         ThrowHelper.ThrowIfNull(t2);
         ThrowHelper.ThrowIfNull(t3);
@@ -78,9 +125,22 @@ public class EitherAttribute : Attribute
         ThrowHelper.ThrowIfNull(t6);
     }
 
-    public EitherAttribute(Type t0, Type t1, Type t2, Type t3, Type t4, Type t5, Type t6, Type t7)
+    /// <summary>
+    /// Initializes a new instance of the <see cref="EitherAttribute"/> class.
+    /// </summary>
+    /// <remarks>
+    /// Depending on nullable reference type scope, reference type may represent either nullable or non-nullable type.
+    /// When <c>nullable</c> is enabled, reference type is not nullable, otherwise it is considered nullable (<c>?</c>).
+    /// </remarks>
+    /// <param name="t1">Type of first value.</param>
+    /// <param name="t2">Type of second value.</param>
+    /// <param name="t3">Type of third value.</param>
+    /// <param name="t4">Type of fourth value.</param>
+    /// <param name="t5">Type of fifth value.</param>
+    /// <param name="t6">Type of sixth value.</param>
+    /// <param name="t7">Type of seventh value.</param>
+    public EitherAttribute(Type t1, Type t2, Type t3, Type t4, Type t5, Type t6, Type t7)
     {
-        ThrowHelper.ThrowIfNull(t0);
         ThrowHelper.ThrowIfNull(t1);
         ThrowHelper.ThrowIfNull(t2);
         ThrowHelper.ThrowIfNull(t3);
@@ -88,5 +148,32 @@ public class EitherAttribute : Attribute
         ThrowHelper.ThrowIfNull(t5);
         ThrowHelper.ThrowIfNull(t6);
         ThrowHelper.ThrowIfNull(t7);
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="EitherAttribute"/> class.
+    /// </summary>
+    /// <remarks>
+    /// Depending on nullable reference type scope, reference type may represent either nullable or non-nullable type.
+    /// When <c>nullable</c> is enabled, reference type is not nullable, otherwise it is considered nullable (<c>?</c>).
+    /// </remarks>
+    /// <param name="t1">Type of first value.</param>
+    /// <param name="t2">Type of second value.</param>
+    /// <param name="t3">Type of third value.</param>
+    /// <param name="t4">Type of fourth value.</param>
+    /// <param name="t5">Type of fifth value.</param>
+    /// <param name="t6">Type of sixth value.</param>
+    /// <param name="t7">Type of seventh value.</param>
+    /// <param name="t8">Type of eighth value.</param>
+    public EitherAttribute(Type t1, Type t2, Type t3, Type t4, Type t5, Type t6, Type t7, Type t8)
+    {
+        ThrowHelper.ThrowIfNull(t1);
+        ThrowHelper.ThrowIfNull(t2);
+        ThrowHelper.ThrowIfNull(t3);
+        ThrowHelper.ThrowIfNull(t4);
+        ThrowHelper.ThrowIfNull(t5);
+        ThrowHelper.ThrowIfNull(t6);
+        ThrowHelper.ThrowIfNull(t7);
+        ThrowHelper.ThrowIfNull(t8);
     }
 }
