4a0cf9ad224c16c5244dfe76eb444b6a25a377ea
Zdenek Havlin <wdolek@hotmail.com>
2023-04-29 23:43:59 +0200

    Add moar XML docs

--- src/W4k.Either.Abstractions/ThrowHelper.cs
+++ src/W4k.Either.Abstractions/ThrowHelper.cs
@@ -4,11 +4,24 @@ using System.Runtime.CompilerServices;
 
 namespace W4k.Either.Abstractions;
 
+/// <summary>
+/// Provides helper methods for throwing exceptions related to invalid states.
+/// </summary>
 public static class ThrowHelper
 {
+    /// <summary>
+    /// Throws an <see cref="InvalidOperationException"/> indicating an invalid state.
+    /// </summary>
+    /// <exception cref="InvalidOperationException">Thrown when this method is called, indicating an invalid state.</exception>
     [DoesNotReturn]
     public static void ThrowOnInvalidState() => throw new InvalidOperationException();
 
+    /// <summary>
+    /// Throws an <see cref="InvalidOperationException"/> indicating an invalid state and returns a value of type <typeparamref name="T"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of the value to return. This method will never return a value, as it always throws an exception.</typeparam>
+    /// <returns>This method does not return a value; it always throws an exception.</returns>
+    /// <exception cref="InvalidOperationException">Thrown when this method is called, indicating an invalid state.</exception>
     [DoesNotReturn]
     public static T ThrowOnInvalidState<T>() => throw new InvalidOperationException();
 
