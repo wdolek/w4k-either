140af4ac22f11d7e00110c8d45d06a25508ae05b
Zdenek Havlin <wdolek@hotmail.com>
2023-06-08 23:09:22 +0200

    Add tests for `Bind` and `Map`

---
+++ tests/W4k.Either.CodeGeneration.IntegrationTests/GenericUsage/MapTests.cs
@@ -0,0 +1,18 @@
+﻿namespace W4k.Either.CodeGeneration.GenericUsage;
+
+public class MapTests
+{
+    [Fact]
+    public void MapToAnotherType()
+    {
+        NotNullEither<Scrooge, Unit> scrooge = new Scrooge(Money: 315_360_000_000_000_000);
+
+        // Scrooge -> Huey
+        var huey = scrooge.Map<Huey>(s => new Huey(s!));
+
+        Assert.IsType<NotNullEither<Huey, Unit>>(huey);
+        Assert.IsType<Huey>(huey.Case);
+    }
+
+    private record Huey(Scrooge Uncle);
+}
